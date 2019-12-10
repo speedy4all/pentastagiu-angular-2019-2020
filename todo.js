56ea094db44f617c34b33a061c5584b040cbc877
albog <alboghardau@gmail.com>
2019-12-10 19:49:34 +0200

    todo list edits

---
+++ todo.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {string} name 
+ * @param {Date} date 
+ */
+function Todo(name, date) {
+    this.name = name;
+    this.data = date;
+    this.finished = false;
+  };
+  
+  /**
+   * @close task
+   */
+  Todo.prototype.finish = function() {
+    this.finished = true;
+  };
+
+  exports.Todo = Todo;
