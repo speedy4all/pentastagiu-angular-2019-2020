763a9b5f9ecbcf19a08f755b534cb95b8ac284cb
RebecaN <rebecanimat@yahoo.com>
2019-12-11 15:42:02 +0200

    Lesson3

--- todolist.js
+++ todolist.js
@@ -12,8 +12,6 @@ tasks.add(new Todo("Go to shopping", new Date("2019-12-05")));
 tasks.add(new Todo("Go to movie", new Date("2019-12-12")));
 tasks.add(new Todo("Dentist appointment", new Date("2019-12-20")));
 
-
-
 tasks.listAll();
 
 const persons = [
