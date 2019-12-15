85dc75df6e055e29740c867b61b8cfc65ea9b409
crlupu <clupu140@gmail.com>
2019-12-15 18:41:42 +0200

    update index tests

--- todos.js
+++ todos.js
@@ -5,17 +5,17 @@ Todos.prototype.add = function (todo) {
     this.todos.push(todo);
 };
 Todos.prototype["delete"] = function (index) {
-    if (index > -1 && index < this.todos.length) {
+    if (index > -1) {
         this.todos.splice(index, 1);
     }
 };
 Todos.prototype.modify = function (index, name, date, description) {
-    if (index > -1 && index < this.todos.length) {
+    if (index > -1) {
         this.todos[index].modify(name, date, description);
     }
 };
 Todos.prototype.finalize = function (index) {
-    if (index > -1 && index < this.todos.length) {
+    if (index > -1) {
         this.todos[index].finalize();
     }
 };
