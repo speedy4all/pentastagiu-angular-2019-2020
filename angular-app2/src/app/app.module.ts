88e815d30b118fc0fd6fce64af44d8df42d60dc7
istvanreti <istvan_foci5@yahoo.com>
2019-12-24 00:52:48 +0200

    homework 3

---
+++ angular-app2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShellModule } from './shell/shell.module';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    //AppRoutingModule,
+    ShellModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
