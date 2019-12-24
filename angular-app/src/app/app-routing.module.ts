af5dfe6f697e4706f29fbe1e27d849b97268d404
vsDeus <camioane112@gmail.com>
2019-12-24 10:45:48 +0200

    Homework preparations -Small stilying changes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,18 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
