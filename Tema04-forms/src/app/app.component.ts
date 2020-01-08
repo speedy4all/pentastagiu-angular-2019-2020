b6b046f135639dc4d3eccc90d304407189742c63
albog <alboghardau@gmail.com>
2020-01-08 22:22:54 +0200

    Homework completed

--- Tema04-forms/src/app/app.component.ts
+++ Tema04-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tema04-forms';
+  title = 'Tema04 Forms & Pipes';
+
+  formOne: FormGroup;
+  formTwo: FormGroup;
+  formThree: FormGroup;
+
+  constructor(private form: FormBuilder){
+    this.createFormOne();
+    this.createFormTwo();
+    this.createFormThree();
+  }
+
+  saveForm(form){
+    console.log(form);
+  }
+
+  createFormOne(){
+      this.formOne = this.form.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      });
+  }
+
+  createFormTwo(){
+    this.formTwo = this.form.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.minLength(8),
+      ])],
+      email: ['', Validators.required],
+    });
+  }
+
+  createFormThree(){
+    this.formThree = this.form.group({
+      text: ['This is a text']
+    });
+  }
 }
--- Tema04-forms/src/app/app.module.ts
+++ Tema04-forms/src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpaceThreePipe } from './space-three.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpaceThreePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
