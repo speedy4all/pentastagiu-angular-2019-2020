78ec2075b815234b79230075979f17740e8edc06
albog <alboghardau@gmail.com>
2020-01-15 21:08:15 +0200

    Homework 5 completed

--- Tema05-Components/src/app/home/home.component.ts
+++ Tema05-Components/src/app/home/home.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoItemComponent } from '../shared-components/todo-item/todo-item.component';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent{
-
-  text: string;
+  title = "Todo App";
+  increment = 0;
+  taskItem = [];
 
   constructor() {
 
-    this.text = "abcd";
   }
 
   addTask(){
-    this.text = "bla bla";
-    console.log(this.text);
+    this.increment++;
+    this.taskItem.push("Task : "+this.increment);
   }
 
+  deleteTask(position){
+    this.taskItem.splice(position, 1);
+  }
 
+  doneTask(position){
+    this.taskItem[position] += " DONE";
+  }
 
 }
--- Tema05-Components/src/app/shared-components/todo-add/todo-add.component.ts
+++ Tema05-Components/src/app/shared-components/todo-add/todo-add.component.ts
@@ -17,6 +17,4 @@ export class TodoAddComponent {
 
   constructor() { }
 
-
-
 }
