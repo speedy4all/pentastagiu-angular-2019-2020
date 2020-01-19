b1a6b6b43abff5ef8075119564ec772bbc5d8209
EddieT21 <eddie.toth@gmail.com>
2020-01-20 00:57:26 +0200

    HW3 rebuilt in separate folder

---
+++ tema-03/todo-app/src/app/shared-components/shared-components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+
+
+
+@NgModule({
+  declarations: [TodoItemComponent, TodoAddComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TodoItemComponent,
+    TodoAddComponent
+  ]
+})
+export class SharedComponentsModule { }
