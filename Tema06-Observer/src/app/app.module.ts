ebbcc385c95a096ada3bb2b6bf3a3ecfc53e4e32
albog <alboghardau@gmail.com>
2020-01-22 19:48:30 +0200

    CounterService CSS and CounterComponent

--- Tema06-Observer/src/app/app.module.ts
+++ Tema06-Observer/src/app/app.module.ts
@@ -1,15 +1,26 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageService } from './message.service';
+
+import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
