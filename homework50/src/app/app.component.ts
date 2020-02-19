c07c74afc2a794fb92d130d63adee65dafe951ec
bianc <biancaluca0208@gmail.com>
2020-02-19 14:53:28 +0200

    homework 50

--- homework50/src/app/app.component.ts
+++ homework50/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'homework50';
+  parentTitle: string;
+
+  constructor() {
+    this.parentTitle = '';
+  }
+
+  wasClicked() {
+    this.parentTitle = 'pentastagiu-angular';
+  }
+
 }
